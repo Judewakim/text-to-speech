369c8550ec5768722a8e79cbd0be6ffc9904da94
Jude Wakim <wjude852@icloud.com>
2024-10-05 15:43:57 +0000

    version 3 not complete. audio bucket is still public. Cloudfront not displaying webpage

--- texttospeech/text-to-speech-tf.tf
+++ texttospeech/text-to-speech-tf.tf
@@ -29,10 +29,10 @@ resource "aws_s3_bucket_cors_configuration" "firstbucketcors" {
 resource "aws_s3_bucket_public_access_block" "s3_public_access" {
   bucket = aws_s3_bucket.firstbucket.id
 
-  block_public_acls   = false
-  ignore_public_acls  = false
-  block_public_policy = false
-  restrict_public_buckets = false
+  block_public_acls   = true
+  ignore_public_acls  = true
+  block_public_policy = true
+  restrict_public_buckets = true
 }
 
 #s3 bucket website config
@@ -53,7 +53,9 @@ resource "aws_s3_bucket_policy" "firstbucketpolicy" {
     Statement = [
       {
         Effect    = "Allow"
-        Principal = "*"
+        Principal = {
+          AWS = "${aws_cloudfront_origin_access_identity.oai.iam_arn}" //or just put back to Principal = {"*"}
+        }
         Action    = [
           "s3:GetObject", 
           "s3:PutObject"
@@ -75,6 +77,56 @@ resource "aws_s3_bucket_notification" "bucket_notification" {
   }
 
   depends_on = [ aws_lambda_permission.allow_bucket ]
+}
+
+# cloudfront distro
+resource "aws_cloudfront_distribution" "distro" {
+  origin {
+    domain_name = "${aws_s3_bucket.firstbucket.bucket_regional_domain_name}"
+    origin_id   = "S3-bucketforfirstaiprojecttf"
+
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.oai.cloudfront_access_identity_path
+    }
+  }
+
+  enabled             = true
+  is_ipv6_enabled      = true
+  default_root_object  = "index.html"
+
+  default_cache_behavior {
+    target_origin_id       = "S3-bucketforfirstaiprojecttf"
+    viewer_protocol_policy = "redirect-to-https"
+
+    allowed_methods = ["GET", "HEAD"]
+    cached_methods  = ["GET", "HEAD"]
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
+
+# CloudFront distribution to serve index.html
+resource "aws_cloudfront_origin_access_identity" "oai" {
+  comment = "OAI for accessing the S3 bucket"
 }
 
 #Lamda & IAM policy-- straight from lamda_function registry.terraform.io
@@ -289,6 +341,3 @@ resource "aws_s3_bucket_policy" "audiostoragebucketpolicy" {
 output "api_url" {
   value = "${aws_api_gateway_deployment.deployment.invoke_url}/resource"
 }
-
-
-#presigned url for audio file bucket
